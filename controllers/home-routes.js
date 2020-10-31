b469866ebe5f43b932d56ff067d9a9fb3948a97d
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-10-31 16:56:18 -0400

    added single-post.handlebars template with a route to display the requested post along with any comments and a form to add a comment; added route to home-routes.js in controllers to render the single-post page, getting the rqested post from the clicked comment value; verified that page is rendered correctly

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,6 +46,52 @@ router.get('/', (req, res) => {
         });
     });
 
+// Route to render a single post page using Sequelize findOne route
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', { post });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 // Route to render the login page
 router.get('/login', (req, res) => {
     // if a session is already detected, reroute to the homepage
