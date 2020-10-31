49ebd7cea9bd0ceecfb858314f57f84d6db95100
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-10-31 10:43:08 -0400

    modified home-routes.js and homepage.handlebars to get the posts and render then in an unordered list on the home page; verified that homepage renders with several posts as expected

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,46 @@
 //Dependencies
 // Router
 const router = require('express').Router();
+// Sequelize and the Post, User, and Comment Models
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models')
 
 // Route to get the homepage
 router.get('/', (req, res) => {
-  res.render('homepage');
-});
+    Post.findAll({
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+            // create an array for the posts, using the get method to trim extra sequelize object data out
+          const posts = dbPostData.map(post => post.get({ plain: true }));
+          // pass the posts into the homepage template
+          res.render('homepage', { posts });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
 
 // Export the router
 module.exports = router;
