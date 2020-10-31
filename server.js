61c75b07e72bac9433c91fc002130a2aeddb7bdd
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-10-31 09:00:42 -0400

    added nodemon as a dev dependency and handlebars for the template engine; added a provided stylesheet to a new public folder and added an express.static line to server.js to serve the static files; added a views folder and the handlebar layouts/main.handlebar file as well as a simple homepage.handlebars file in views

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 // Server for the Just Tech News project
 
 // Dependencies
+// path module
+const path = require('path');
 // Express.js server
 const express = require('express');
 // All routes as defined and exported in the index.js file in the routes folder
 const routes = require('./routes/index');
 // Sequelize connection to the database
 const sequelize = require('./config/connection');
+// Handlebars template engine for front-end
+const exphbs = require('express-handlebars');
+
+// Initialize handlebars for the html templates
+const hbs = exphbs.create({});
 
 // Initialize the server
 const app = express();
 // Define the port for the server
 const PORT = process.env.PORT || 3001;
 
+// Give the server a path to the public directory for static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set handlebars as the template engine for the server
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // Have Express parse JSON and string data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Turn on the routes
+// Give the server the path to the routes
 app.use(routes);
+
 
 // Turn on connection to db and then to the server
 // force: true to reset the database and clear all values, updating any new relationships
