f8a33dac4a341ea7215df2b443d1fbe8038fa668
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-11-01 20:12:33 -0500

    added dashboard.handlebars with a (non-functional as yet) form to add a new post; added dashboard-routes.js to provide routes for the dashboard page and added them to the index router; added dashboard link to main.handlebars nav section for logged in users, and modified login.js to redirect users to their dashboard page when they log in

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,14 @@
 const router = require('express').Router();
 // API routes folder
 const apiRoutes = require('./api');
-// The handlebars routes
+// The handlebars homepage and single page routes
 const homeRoutes = require('./home-routes.js');
+// The handlebars dashboard routes
+const dashboardRoutes = require('./dashboard-routes.js');
 
-// Define a path for the handlebars html routs
+// Define paths for the handlebars html routes
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // Define the path for the server for the API routes
 router.use('/api', apiRoutes);
